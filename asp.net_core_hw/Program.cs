1c455ae9cabede8eb0ff08a089aa41781e351fa9
Alex <sin27san@gmail.com>
2022-11-07 22:34:53 +0300

    Представим, что мы работаем с api системы блогов. Перед нами поставлена задача получить несколько постов сразу, обработать содержимое ответа и записать результат в текстовый файл.

--- asp.net_core_hw/Program.cs
+++ asp.net_core_hw/Program.cs
@@ -16,9 +16,65 @@ namespace asp.net_core_hw
 
         static async Task Main()
         {
-            
+            try
+            {
+                List<Task<Request>> taskList = new List<Task<Request>>();//создаем коллекцию задач
+                HttpClient client = new HttpClient();
+
+                for (int id = 4; id <= 13; id++)
+                {
+                    taskList.Add(Response(client, id.ToString()));
+                }
+
+                if (Task.WhenAll(taskList).Wait(10_000))//статический метод ждет пока вся наша колллекция методов выполнится
+                {
+                    StringBuilder sb = new StringBuilder();
+
+                    foreach (var text in taskList)
+                    {
+                        sb.Append(text.Result.userId + "\n" + text.Result.id + "\n" + text.Result.title + "\n" + text.Result.body + "\n\n");
+                    }
+
+                    using (StreamWriter sw = new StreamWriter("result.txt"))
+                    {
+                        sw.WriteLine(sb.ToString());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Прерывание программы!");
+                }
+
+                Console.WriteLine("Программа завершена!");
+                Console.ReadKey();
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+
         }
 
-        
+
+
+        static async Task<Request> Response(HttpClient client, string id)
+        {
+            HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/posts/" + id);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Request request = JsonConvert.DeserializeObject<Request>(responseBody);
+            return request;
+        }
+
+        public class Request
+        {
+            public string userId { get; set; }
+            public string id { get; set; }
+            public string title { get; set; }
+            public string body { get; set; }
+        }
+
+
     }
 }
