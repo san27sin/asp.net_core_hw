14279e58e5bd60c8dee27c40064fabed399fa27f
Alex <sin27san@gmail.com>
2022-11-07 22:31:00 +0300

    Add project files.

---
+++ asp.net_core_hw/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace asp.net_core_hw
+{
+    internal class Program
+    {      
+
+        static async Task Main()
+        {
+            
+        }
+
+        
+    }
+}
